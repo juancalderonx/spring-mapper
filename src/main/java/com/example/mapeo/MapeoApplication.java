192e7ced6954219b9932269161dcd213d930ad0e
juancalderonx <juanescalderon12@gmail.com>
2024-01-12 01:29:08 -0500

    feat(): initial commit

---
+++ src/main/java/com/example/mapeo/MapeoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapeoApplication.class, args);
+    }
+
+}
